997aa01c47567a1232eb3972cdcc6990f45df27c
tombolton96 <tbolton96@icloud.com>
2021-04-12 01:26:12 +0100

    Update service worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -2,14 +2,8 @@ self.addEventListener('install', function (e) {
     e.waitUntil(
       caches.open('dwylapp').then(function (cache) {
         return cache.addAll([
-          '/',
-          '/manifest.webmanifest',
-          '/*.js',
-          '/Icon-48.png',
-          '/Icon-72.png',
-          '/Icon-144.png',
-          '/Icon-167.png',
-          '/*.css',
+          './',
+          './index.html',
         ]);
       })
     );
